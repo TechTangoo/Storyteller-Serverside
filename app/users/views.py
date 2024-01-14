a9a6ccdf6cdee0d4ba718cb6c4aa979b3e60b7ea
priyankaj04 <priyankaj2002o4@gmail.com>
2024-01-14 21:45:01 +0530

    user routes

--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,25 @@
 from app.users import bp
 
 
-@bp.route("/new", methods=['GET'])
+@bp.route("/register", methods=['POST'])
 def new_user():
     return "New user hi"
+
+
+@bp.route("/login", methods=['GET'])
+def login_user():
+    print("lgon user")
+    return "authorization"
+
+# @bp.route("/<string:userid>", methods=['GET'])
+# def get_user():
+#     return "get user details"
+
+# @bp.route("/<string:userid>", methods=['PUT'])
+# def update_user():
+#     """Function printing python version."""
+#     return "put user details"
+
+# @bp.route("/remove", methods=['PATCH'])
+# def delete_user():
+#     return "New user hi"

ed7844c1d5131d8a4c357cfcc7821e0da86a9291
Haarvish <haarvish@gmail.com>
2024-01-14 13:11:05 +0530

    feat: Made new route for user data

--- app/users/views.py
+++ app/users/views.py
@@ -4,3 +4,8 @@ from app.users import bp
 @bp.route("/new", methods=['GET'])
 def new_user():
     return "New user hi"
+
+
+@bp.route("/data", methods=['GET'])
+def user_data():
+    return "New data"

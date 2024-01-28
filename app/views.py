e0abe7a9d4563606668cb979c22461ecc0e4215e
priyankaj04 <priyankaj2002o4@gmail.com>
2024-01-28 16:47:12 +0530

    bugfix

--- app/views.py
+++ app/views.py
@@ -34,12 +34,24 @@ def register():
 
 @app.route("/insert", methods=['POST'])
 def insert_data():
-    data = request.get_json() or {}
-    email = data.get('email')
-    password = data.get('password')
-    if not email or not password:
-        return jsonify({'error': 'Email and password are required'}), 400
+    try:
+        data = request.get_json() or {}
+        email = data.get('email')
+        password = data.get('password')
+        fullname = data.get('fullname')
+        if not email or not password:
+            return jsonify({'error': 'Email and password are required'}), 400
 
-    user_data = {'email': email, 'passcode': password}
-    user_insert_response = Supabase.table('User').insert(user_data).execute()
-    print(user_insert_response)
+        user_data = {'email': email, 'passcode': password, 'user_name': fullname}
+        # Assuming Supabase is properly imported and initialized
+        user_data_res = Supabase.table('User').insert(user_data).execute()
+
+        if user_data_res.data:  # Assuming data is not None or False
+            response_data = {'success': True, 'message': 'Data inserted successfully'}
+            return jsonify(response_data), 201
+        else:
+            response_data = {'success': False,'message': 'Failed to insert data'}
+            return jsonify(response_data), 500
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
